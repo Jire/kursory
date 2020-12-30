86caf702ae7b52daabf427b33ef0af5bf635e782
Jire <thomasgnappo@gmail.com>
2020-12-29 21:53:44 -0500

    build: Gradle use jcenter, chronicle-core dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,20 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.4.21"
+	kotlin("jvm") version "1.4.21"
 }
 
 group = "org.jire"
 version = "0.1.0"
 
 repositories {
-    mavenCentral()
+	jcenter()
 }
 
-
+dependencies {
+	implementation("net.openhft:chronicle-core:2.20.126")
+}
 
 tasks.withType<KotlinCompile>() {
-    kotlinOptions.jvmTarget = "1.8"
+	kotlinOptions.jvmTarget = "1.8"
 }
