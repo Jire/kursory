a7cc667c3d3905e1407ee796b042668d0c91a3ce
Jire <thomasgnappo@gmail.com>
2020-12-31 08:20:32 -0500

    build: JMH support

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
 	kotlin("jvm") version "1.4.21"
+	id("me.champeau.gradle.jmh") version "0.5.2"
 }
 
 group = "org.jire"
@@ -17,9 +18,19 @@ repositories {
 }
 
 dependencies {
-	implementation("net.openhft:chronicle-core:2.20.126")
+	implementation("net.openhft", "chronicle-core", "2.20.126")
+	
+	val jmhVersion = "1.27"
+	implementation("org.openjdk.jmh", "jmh-core", jmhVersion)
+	annotationProcessor("org.openjdk.jmh", "jmh-generator-annprocess", jmhVersion)
+	jmh("org.openjdk.jmh", "jmh-core", jmhVersion)
+	jmhAnnotationProcessor("org.openjdk.jmh", "jmh-generator-annprocess", jmhVersion)
 }
 
 tasks.withType<KotlinCompile> {
 	kotlinOptions.jvmTarget = "1.8"
+}
+
+jmh {
+	duplicateClassesStrategy = DuplicatesStrategy.EXCLUDE
 }
