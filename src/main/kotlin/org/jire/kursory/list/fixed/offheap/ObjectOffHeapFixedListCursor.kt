4ad1961982fb4470a8c7563619c68b48e7923ddb
Jire <thomasgnappo@gmail.com>
2020-12-31 08:26:34 -0500

    chore: added per-file license

--- src/main/kotlin/org/jire/kursory/list/fixed/offheap/ObjectOffHeapFixedListCursor.kt
+++ src/main/kotlin/org/jire/kursory/list/fixed/offheap/ObjectOffHeapFixedListCursor.kt
@@ -1,3 +1,9 @@
+/*
+ *  This Source Code Form is subject to the terms of the Mozilla Public
+ *  * License, v. 2.0. If a copy of the MPL was not distributed with this
+ *  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 package org.jire.kursory.list.fixed.offheap
 
 import org.jire.kursory.list.fixed.FixedListObjectCursor
@@ -7,6 +13,6 @@ class ObjectOffHeapFixedListCursor<T : Any>(
 ) : AbstractOffHeapFixedListCursor(list),
 	FixedListObjectCursor<T> {
 	
-	override val next get() = list[index]!!
+	override val next get() = list[index]
 	
 }
